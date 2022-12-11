9ddab4d7218a6ff6456d9bcce88ec75eaf1ec5fa
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2022-12-11 22:59:42 +0530

    Front-End part of the social media app

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const Followers = [
+    {name: 'Amit Gorai', username: 'AmitGorai', img: img1},
+    {name: 'Sachin Dutta', username: 'SDutta', img: img2},
+    {name: 'Sayan Dey', username: 'sd', img: img3},
+    {name: 'Jharna Mandal', username: 'Jmandal', img: img4},
+];
