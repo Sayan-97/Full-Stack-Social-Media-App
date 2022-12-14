cd11f756df0e71da740d9cb2ec410cd383684c3c
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2022-12-14 14:28:56 +0530

    Completed the frontend part of the project

---
+++ src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+  {
+    name: "Minions",
+    shares: 97,
+  },
+  {
+    name: "Avengers",
+    shares: 88.5,
+  },
+  {
+    name: "Panda",
+    shares: 75.5,
+  },
+  {
+    name: "Reactjs",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "Need For Speed",
+    shares: 20,
+  },
+];
